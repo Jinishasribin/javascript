1d4ebe7adefe950a7845dd1358396b4c6474ef52
Jinishasribin <132832015+Jinishasribin@users.noreply.github.com>
2023-05-13 19:34:47 +0530

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,34 @@
+console.log("welcome to js")
+//addition
+let x = 23
+let y = 34
+console.log (x+y);
+//subtraction 
+let a = 55
+let b = 36
+console.log ("a-b");
+//maltiplication
+let m =34
+let n =25
+console.log("m*n");
+//division
+let d =15
+let e =5
+console.log("d/e");
+//percentile
+let k =17
+let l =3
+console.log("17%3");
+
+//operations 
+let j = 45
+let o =34
+let w = j+o
+let p = 23
+let q = w*p
+let r = 3
+let s = q-r
+console.log(w);
+console.log(q);
+console.log(s);
+
